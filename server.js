02b0aba9e5e039853fa1453c40a4cfc209390365
theglitch219 <147455524+theglitch219@users.noreply.github.com>
2026-08-18 16:10:35 +0100

    Change static directory from 'public' to 'Public'

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { Pool } = pg;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 app.use(express.json({limit:"1mb"}));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"Public")));
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
